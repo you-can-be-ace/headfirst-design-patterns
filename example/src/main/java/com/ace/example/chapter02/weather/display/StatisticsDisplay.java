20426a8b6b9aac2217b99ddc29102e232ffd34cb
Meun <hkb8322@naver.com>
2024-06-06 22:47:10 +0900

    [Chapter02] 옵저버 패턴 (#1)
    * 푸시 방식으로 기상 스테이션 구현

---
+++ example/src/main/java/com/ace/example/chapter02/weather/display/StatisticsDisplay.java
@@ -0,0 +1,41 @@
+package com.ace.example.chapter02.weather.display;
+
+import com.ace.example.chapter02.weather.WeatherData;
+import com.ace.example.chapter02.weather.observer.Observer;
+
+public class StatisticsDisplay implements Observer, DisplayElement {
+
+	private float maxTemp = 0.0f;
+	private float minTemp = 200;
+	private float tempSum= 0.0f;
+	private int numReadings;
+	private WeatherData weatherData;
+
+	public StatisticsDisplay(WeatherData weatherData) {
+		this.weatherData = weatherData;
+
+		weatherData.registerObserver(this);
+	}
+
+	@Override
+	public void update(float temp, float humidity, float pressure) {
+		tempSum += temp;
+		numReadings++;
+
+		if (temp > maxTemp) {
+			maxTemp = temp;
+		}
+ 
+		if (temp < minTemp) {
+			minTemp = temp;
+		}
+
+		display();
+	}
+
+	@Override
+	public void display() {
+		System.out.println("평균/최대/최저 온도 = " + (tempSum / numReadings)
+			+ "/" + maxTemp + "/" + minTemp);
+	}
+}
