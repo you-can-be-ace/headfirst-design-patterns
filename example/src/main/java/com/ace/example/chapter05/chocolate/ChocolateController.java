54e25e4ae37a8d21f22a0507a4a9aa96608ca38d
Meun <hkb8322@naver.com>
2024-06-28 23:35:10 +0900

    [Chapter05] 싱글톤 패턴 (#6)
    * 싱글톤 패턴 예제 코드 업로드
      -> 상속, 열거형, thread-safe 관련

---
+++ example/src/main/java/com/ace/example/chapter05/chocolate/ChocolateController.java
@@ -0,0 +1,14 @@
+package com.ace.example.chapter05.chocolate;
+ 
+public class ChocolateController {
+
+	public static void main(String args[]) {
+		ChocolateBoiler boiler = ChocolateBoiler.getInstance();
+
+		boiler.fill();
+		boiler.boil();
+		boiler.drain();
+
+		ChocolateBoiler boiler2 = ChocolateBoiler.getInstance();
+	}
+}
