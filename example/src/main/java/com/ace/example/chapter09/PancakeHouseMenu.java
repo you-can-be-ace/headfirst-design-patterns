5846ddca7a22bd51e4a69f91a936ae0dbc4229bd
HYEMSTER <hyemsterr@gmail.com>
2024-07-27 21:59:18 +0900

    [CHAPTER-09] 서로 다른 2개의 식당

---
+++ example/src/main/java/com/ace/example/chapter09/PancakeHouseMenu.java
@@ -0,0 +1,28 @@
+package com.ace.example.chapter09;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PancakeHouseMenu {
+
+  List<MenuItem> menuItems;
+
+  public PancakeHouseMenu() {
+    menuItems = new ArrayList<MenuItem>();
+
+    addItem("K&B 팬케이크 세트", "스크램블 에그와 토스트가 곁들여진 팬케이크", true, 2.99);
+    addItem("레귤려 팬케이크 세트", "달걀 프라이와 소시지가 곁들여진 팬케이크", false, 2.99);
+    addItem("블루베리 팬케이크 세트", "신선한 블루베리와 블루베리 시럽으로 만든 팬케이크", true, 3.49);
+    addItem("와플", "취향에 따라 블루베리나 딸기를 얹을 수 있는 와플", true, 3.59);
+  }
+
+  public void addItem(String name, String description, boolean vegetarian, double price) {
+    var menuItem = new MenuItem(name, description, vegetarian, price);
+    menuItems.add(menuItem);
+  }
+
+  public List<MenuItem> getMenuItems() {
+    return menuItems;
+  }
+
+}
